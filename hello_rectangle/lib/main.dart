1f3b1dfb6b33c9ba457874c57a785f4b218f73aa
Aseem Srivastava <as3eem@gmail.com>
2018-06-02 15:15:38 +0530

    Basic learning experiments in flutter

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 
+class HelloRectangle extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: Container(
+        color: Colors.green,
+        height: 400.0,
+        width: 300.0,
+        child: new Center(
+          child: Text('HEllO RECTANGLE',style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+    ),
+    );
+  }
+}
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Rectangle'),
+          backgroundColor: Colors.pinkAccent   ,
+          title: Text('Experiments with Flutter'),
         ),
-          body: Container(
-          color: Colors.red,
-        ),
+          body: HelloRectangle(),
       ),
     ),
   );
